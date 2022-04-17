650f769303c3c16c5280c99abd4c06fd8bae9c1e
Vikramaditya Singh <vikramadityascode@gmail.com>
2022-04-17 22:23:49 +0530

    text change

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -5,11 +5,23 @@ const Home = () => {
     <div>
       <div className="container">
         <br />
-        <h1 className="title">Home</h1>
+        <br />
+
         <div className="content">
-          <p>Lorem Ipsum</p>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industrys standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
         </div>
-        <h2 className="title"> Sorts</h2>
+        <h1 className="title"> Sorts</h1>
+        <br />
         <CardGallery data={manifest} />
       </div>
     </div>
