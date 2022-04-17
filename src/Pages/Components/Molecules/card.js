11ec0a810de5ea0279af2c3655bfd4df37a91945
Vikramaditya Singh <vikramadityascode@gmail.com>
2022-04-17 22:19:13 +0530

    card changes

--- src/Pages/Components/Molecules/card.js
+++ src/Pages/Components/Molecules/card.js
@@ -1,25 +1,16 @@
 import { Link } from "react-router-dom";
+import imageMap from "../../imageMap";
 
 const Card = ({ data }) => {
+  //   const temp = images("./bubble.svg");
   return (
     <Link to={`/sort/${data.url}`}>
       <div className="card">
-        <div clclassNameass="card-image">
-          <figure className="image is-4by3">
-            <img
-              src="https://bulma.io/images/placeholders/1280x960.png"
-              alt=""
-            />
-          </figure>
-        </div>
         <div className="card-content">
           <div className="media">
             <div className="media-left">
               <figure className="image is-48x48">
-                <img
-                  src="https://bulma.io/images/placeholders/96x96.png"
-                  alt=""
-                />
+                <img src={imageMap.user} alt="logo" />
               </figure>
             </div>
             <div className="media-content">
---
+++ src/Pages/imageMap.js
@@ -0,0 +1,7 @@
+import user from "../Resources/images/user.png";
+
+const imageMap = {
+  user: user,
+};
+
+export default imageMap;
